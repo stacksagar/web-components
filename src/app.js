6fcb22c28783dfdb876c7e3a999e0ecf17af80a4
Sagar Roy <stacksagar@gmail.com>
2023-07-23 15:46:58 +0600

    configuration done

---
+++ src/app.js
@@ -0,0 +1,31 @@
+"use strict";
+class CustomButton extends HTMLElement {
+    constructor() {
+        super();
+        this.shadow = this.attachShadow({ mode: "open" });
+        this.name = "Sagar";
+    }
+    connectedCallback() {
+        const button = document.createElement("button");
+        button.innerHTML = this.innerHTML;
+        this.shadow.append(this.styles());
+        this.shadow.append(button);
+        console.log("not working bro");
+    }
+    disconnectedCallback() { }
+    attributeChangedCallback() { }
+    styles() {
+        const style = document.createElement("style");
+        style.innerText = `
+      button {
+          background: orange;
+          color: white;
+          padding: 10px 20px;
+          background: black;
+          border: 4px;
+        }
+      `;
+        return style;
+    }
+}
+customElements.define("custom-button", CustomButton);
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,30 +1,35 @@
 class CustomButton extends HTMLElement {
+  shadow: ShadowRoot;
+  name: string;
   constructor() {
     super();
     this.shadow = this.attachShadow({ mode: "open" });
+    this.name = "Sagar";
   }
 
   connectedCallback() {
     const button = document.createElement("button");
     button.innerHTML = this.innerHTML;
-    this.shadow.append(this.#styles());
+    this.shadow.append(this.styles());
     this.shadow.append(button);
+
+    console.log("not working bro");
   }
 
   disconnectedCallback() {}
   attributeChangedCallback() {}
 
-  #styles() {
+  private styles() {
     const style = document.createElement("style");
     style.innerText = `
-    button {
-        background: orange;
-        color: white;
-        padding: 10px 20px;
-        background: black;
-        border: 4px;
-      }
-    `;
+      button {
+          background: orange;
+          color: white;
+          padding: 10px 20px;
+          background: black;
+          border: 4px;
+        }
+      `;
 
     return style;
   }
